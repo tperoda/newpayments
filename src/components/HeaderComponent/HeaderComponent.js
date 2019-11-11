8d59e079b92c4d5c1b80d3a71c16b753b615edf1
Tyler <tpedora@me.com>
2019-11-11 16:10:36 -0700

    feat: creating base code and sass setup

---
+++ src/components/HeaderComponent/HeaderComponent.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { Menu } from "semantic-ui-react";
+import "./styles.scss";
+
+const HeaderComponent = () => {
+  return (
+    <Menu pointing className="payment-header">
+      <h4>Payment Calculator</h4>
+    </Menu>
+  );
+};
+
+export default HeaderComponent;
